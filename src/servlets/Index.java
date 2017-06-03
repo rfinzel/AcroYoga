f61e65f2ef0c5ad29eb34c8c3f9f30d6297d0117
CelinaSkywalker <celinalandgraf@web.de>
2017-06-03 20:02:07 +0200

    Login Versuch(funktioniert noch nicht)

--- src/servlets/Index.java
+++ src/servlets/Index.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,6 +14,7 @@ import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -55,22 +57,45 @@ public class Index extends HttpServlet {
 			user = "<li class=\"dropdown\">"
 				   + "<a class=\"dropdown-toggle\" href=\"#\" data-toggle=\"dropdown\">Sign In <strong class=\"caret\"></strong></a>"
 				   + "<div class=\"dropdown-menu\" style=\"padding: 15px; padding-bottom: 0px;\">"
-				   + "<form method=\"post\" action=\"login\" accept-charset=\"UTF-8\">" 
+				   + "<form method=\"post\" action=\"Index\" accept-charset=\"UTF-8\">" 
 				   +			"<input style=\"margin-bottom: 15px;\" type=\"text\" placeholder=\"Username\" id=\"username\" name=\"username\">"
-				+			"<input style=\"margin-bottom: 15px;\" type=\"password\" placeholder=\"Password\" id=\"password\" name=\"password\">"
-				+				"<input style=\"float: left; margin-right: 10px;\" type=\"checkbox\" name=\"remember-me\" id=\"remember-me\" value=\"1\">"
-				+				"<label class=\"string optional\" for=\"user_remember_me\"> Remember me</label>"
-				+				"<input class=\"btn btn-primary btn-block\" type=\"submit\" id=\"sign-in\" value=\"Sign In\">"
-				+				"<label style=\"text-align:center;margin-top:5px\">or</label>"
-                 +               "<input class=\"btn btn-primary btn-block\" type=\"button\" id=\"sign-in-google\" value=\"Sign In with Google\">"
-					+			"<input class=\"btn btn-primary btn-block\" type=\"button\" id=\"sign-in-twitter\" value=\"Sign In with Twitter\">"
-					+		"</form>"
-					+	"</div>"
-					+"</li>";
+				   +			"<input style=\"margin-bottom: 15px;\" type=\"password\" placeholder=\"Password\" id=\"password\" name=\"password\">"
+				   +				"<input style=\"float: left; margin-right: 10px;\" type=\"checkbox\" name=\"remember-me\" id=\"remember-me\" value=\"1\">"
+				   +				"<label class=\"string optional\" for=\"user_remember_me\"> Remember me</label>"
+				   + 				"<input class=\"btn btn-primary btn-block\" type=\"submit\" id=\"sign-in\" value=\"Sign In\">"
+				   +		"</form>"
+				   +	"</div>"
+				   +"</li>";
 			headerText = "AcroYoga";
 		}
 			
-		request.setAttribute("user", user);
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		Cookie loginCookie = null;
+		loginCookie = new Cookie("user", username);
+		
+		//setting cookie to expiry in 30 mins
+		loginCookie.setMaxAge(30*60);
+		response.addCookie(loginCookie);
+		//response.sendRedirect("LoginSuccess.jsp");
+		/*if(username.equals(username) && password.equals(password))
+		{
+			loginCookie = new Cookie("user", username);
+			
+			//setting cookie to expiry in 30 mins
+			loginCookie.setMaxAge(30*60);
+			response.addCookie(loginCookie);
+			response.sendRedirect("LoginSuccess.jsp");
+		}
+		else
+		{
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+			PrintWriter out= response.getWriter();
+			out.println("<font color=red>Either user name or password is wrong.</font>");
+			rd.include(request, response);
+		}*/
+		
+		request.setAttribute("user", username);
 		
 		request.setAttribute("headerText", headerText);
 		
