dddcf8c3b3fa1781889ff2cf0d1230cd447a84c9
rene-laptop <r.finzel@outlook.de>
2017-06-14 15:06:24 +0200

    EventDAO
    Add Event hnzugefügt

--- src/daos/EventDAO.java
+++ src/daos/EventDAO.java
@@ -62,4 +62,40 @@ public class EventDAO {
 		return e;		
 	}
 	
+	public void addEvent(Event e)
+	{
+		conn = conProvider.getConnection();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement("insert into event values(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			pstmt.setInt(1, getNextId());
+			pstmt.setString(2, e.getName());
+			pstmt.setDate(3, e.getTiming());
+			pstmt.setInt(4, e.getRegularity());
+			pstmt.setString(5, e.getPlace());
+			pstmt.setString(6, e.getShortContent());
+			pstmt.setString(7, e.getContent());
+			pstmt.setDouble(8, e.getFee());
+			pstmt.setInt(9, e.getInstructor());
+			pstmt.executeQuery();
+		} catch(SQLException e1) {
+			System.out.println(e1.toString());
+		}
+	}
+	
+	private int getNextId()
+	{
+		int id = 0;
+		conn = conProvider.getConnection();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement("select * from event order by id desc");		
+			ResultSet rs = pstmt.executeQuery();
+			
+			rs.next();
+			id = rs.getInt(1);
+		} catch(SQLException e1) {
+			System.out.println(e1.toString());
+		}
+		
+		return id+1;	
+	}
 }
